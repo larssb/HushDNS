314e1ae0a3e975ef90358300a881cc8b777543a4
Lars Bengtsson <bingchonglars@gmail.com>
2020-05-01 21:55:18 +0200

    Clean-up the HushDNS setup shell script
    - no need for ulimit anymore ... not needed
    - no need for net=host ... avoid security issues

--- unit-deployment/dnscrypt-server/setup.sh
+++ unit-deployment/dnscrypt-server/setup.sh
@@ -3,12 +3,8 @@
 cd "$(dirname "$(readlink -f "$0" || realpath "$0")")"
 
 # Initialize the dnscrypt-server. Pre-requisite for running the full svc. afterwards
-docker run --ulimit nofile=90000:90000 --name=dnscrypt-server -p 443:443/udp -p 443:443/tcp \
---net=host -v /LOCAL_CUSTOM_UNBOUND_CONF_DIR:/opt/unbound/etc/unbound/zones \
+docker run --name=dnscrypt-server -p 443:443/udp -p 443:443/tcp \
+--restart=unless-stopped \
+-v /LOCAL_CUSTOM_DNSCRYPT_SERVER_CONF_DIR/unbound-conf:/opt/unbound/etc/unbound/zones \
+-v /LOCAL_CUSTOM_DNSCRYPT_SERVER_CONF_DIR/keys:/opt/encrypted-dns/etc/keys \
 jedisct1/dnscrypt-server init -N NAME_TO_GIVE_YOUR_DNSCRYPT_SERVER -E WAN_IP_IN_FRONT_OF_THE_dnscrypt-server:443
-
-# Start the dnscrypt-server
-docker start dnscrypt-server
-
-# Update the restart policy of the container
-docker update --restart=unless-stopped dnscrypt-server
