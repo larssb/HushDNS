dd3018f71955f72cb64f6d3b78e907b249879389
Anders Nyvang <nanders@nanders.com>
2020-05-02 10:19:42 +0200

    Docker-compose version of dnscrypt-server

--- stack-deployment/local-dev-test/dnscrypt-server/setup.sh
+++ stack-deployment/local-dev-test/dnscrypt-server/setup.sh
@@ -3,11 +3,7 @@
 cd "$(dirname "$(readlink -f "$0" || realpath "$0")")"
 
 # Initialize the dnscrypt-server. Pre-requisite for running the full svc. afterwards
-docker run --ulimit nofile=90000:90000 --name=dnscrypt-server -p 4443:443/udp -p 4443:443/tcp --network=piholed_static-network \
---ip 172.20.128.3 jedisct1/dnscrypt-server init -N hush.dns -E 172.20.128.3:4443
+docker-compose up -d
 
-# Start the dnscrypt-server
-docker start dnscrypt-server
-
-# Update the restart policy of the container
-docker update --restart=unless-stopped dnscrypt-server
+# Output the log
+docker-compose logs
