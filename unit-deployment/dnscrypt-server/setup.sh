bcc5937505d1d6fb8fc9ede41e139f5d7125a7d5
Anders Nyvang <nanders@nanders.com>
2022-09-19 11:01:58 +0200

    Changed dnscrypt-server to docker-compose
    - Switched to multi-arch image of dnscrypt-server

--- unit-deployment/dnscrypt-server/setup.sh
+++ unit-deployment/dnscrypt-server/setup.sh
@@ -3,8 +3,5 @@
 cd "$(dirname "$(readlink -f "$0" || realpath "$0")")"
 
 # Initialize the dnscrypt-server. Pre-requisite for running the full svc. afterwards
-docker run --name=dnscrypt-server -p 443:443/udp -p 443:443/tcp \
---restart=unless-stopped \
--v /LOCAL_CUSTOM_DNSCRYPT_SERVER_CONF_DIR/unbound-conf:/opt/unbound/etc/unbound/zones \
--v /LOCAL_CUSTOM_DNSCRYPT_SERVER_CONF_DIR/keys:/opt/encrypted-dns/etc/keys \
-jedisct1/dnscrypt-server init -N NAME_TO_GIVE_YOUR_DNSCRYPT_SERVER -E WAN_IP_IN_FRONT_OF_THE_dnscrypt-server:443
+# REMEMBER! to change the volume mounts in the docker-compose.yml
+docker compose run dnscrypt-server init -N NAME_TO_GIVE_YOUR_DNSCRYPT_SERVER -E WAN_IP_IN_FRONT_OF_THE_dnscrypt-server:443
